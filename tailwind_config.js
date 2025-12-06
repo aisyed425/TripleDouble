6b14c34899f4b9365a164fbc492321ccdb2a7312
Ahmed Syed <153319561+aisyed425@users.noreply.github.com>
2025-12-05 22:11:32 -0500

    Update tailwind_config.js

--- tailwind_config.js
+++ tailwind_config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -6,3 +8,5 @@ export default {
   },
   plugins: [],
 }
+
+/** a script to configure tailwind
