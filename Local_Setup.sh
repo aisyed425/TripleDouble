de7f8ef030e4c873aea089b1d5c436dca8f3dd16
Ahmed Syed <153319561+aisyed425@users.noreply.github.com>
2025-12-08 21:08:56 -0500

    Update Local_Setup.sh

--- Local_Setup.sh
+++ Local_Setup.sh
@@ -1,9 +1,9 @@
-#Vite, React, and TypeScrip
+#Vite, React, and TypeScript
 
 npm create vite@latest playoff-legends -- --template react-ts
 cd Triple-Double
 npm install
 
 
-#Run Dev Server
+#Run Dev Srver
 npm run dev
